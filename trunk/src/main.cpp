3f1b44b5345cebdd0b88457747a82d3e91586d26
joe.foley@gmail.com <joe.foley@gmail.com@506386cb-9f3d-4a9b-0b35-aa8e30c3513f>
2011-03-24 22:58:52 +0000

    Blink code now works on Arduino Uno under Eclipse Helios.

---
+++ trunk/src/main.cpp
@@ -0,0 +1,64 @@
+#include <WProgram.h>
+#include <stdlib.h>
+
+extern "C" void __cxa_pure_virtual()
+{
+  cli();
+  for (;;);
+}
+
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+void * operator new(size_t size)
+{
+  return malloc(size);
+}
+
+void operator delete(void * ptr)
+{
+  free(ptr);
+}
+
+void * operator new[](size_t size)
+{
+    return malloc(size);
+}
+
+void operator delete[](void * ptr)
+{
+	if (ptr)
+		free(ptr);
+}
+
+int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);}
+void __cxa_guard_release (__guard *g) {*(char *)g = 1;}
+void __cxa_guard_abort (__guard *) {}
+
+
+void setup() {
+  // initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards:
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(200);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(2000);              // wait for a second
+}
+
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+
+	// IMPORTANT!!!!!!!!!!
+	// The Arduino main() must NEVER NEVER NEVER return
+	return 0;
+}
+
