4cf6b71002a858d602f6e82a60c71c48e5b83339
joe.foley@gmail.com <joe.foley@gmail.com@506386cb-9f3d-4a9b-0b35-aa8e30c3513f>
2012-11-16 11:24:06 +0000

    Updated configs for eclipse Juno.  Builds for .hex files are working for everything except the UNO build.  Investigating.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+/* This is included so that you can use the Arduino .ino files
+ *   it is enabled if you use the UNO/Mega build profile
+ */
 #include <WProgram.h>
 #include <stdlib.h>
 #include<ArduinoRoomba.h>
@@ -6,6 +9,14 @@
 // ArduinoRoomba(rx,tx,dd)
 /// Default arduino pin configuration, RX on pin 3, TX on pin 4, DD on pin 5
 
+void setup() {
+
+}
+
+void loop() {
+	// put something that gets run multiple times
+
+}
 
 int main(void)
 {
