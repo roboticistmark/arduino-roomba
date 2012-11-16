a8a0f0d94d2825624c6a96410338c7356bc0cbdd
joe.foley@gmail.com <joe.foley@gmail.com@506386cb-9f3d-4a9b-0b35-aa8e30c3513f>
2012-11-16 11:35:38 +0000

    Deleted, then recopied the UNO build configuration from the RobbieRobot one.  Now it makes a hex file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 /* This is included so that you can use the Arduino .ino files
  *   it is enabled if you use the UNO/Mega build profile
+ *   Alternatively, you can just put the code you want to call here
+ *   If you use the Arduino style, you'll need to uncomment the setup() and loop() definition
  */
 #include <WProgram.h>
 #include <stdlib.h>
@@ -10,7 +12,7 @@
 /// Default arduino pin configuration, RX on pin 3, TX on pin 4, DD on pin 5
 
 void setup() {
-
+	// put something that gets run at the beginnning
 }
 
 void loop() {
